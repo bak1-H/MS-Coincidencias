7d5fed6d0083043ab52e81fa5bee41033c9ff070
bak1-H <maxi.huerta@duocuc.cl>
2026-05-08 17:45:34 -0400

    first commit

---
+++ src/test/java/com/sanosysalvos/coincidencias/CoincidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.coincidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoincidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
